f6e71b29e6cea2ced4d4d0297f9c5d1872439835
Wall <ilnazamirhanov61@gmail.com>
2023-02-09 18:53:18 +0300

    add board part

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+  <img class="logo" alt="Vue logo" src="./assets/logo.png">
+  <h1 class="h1">Запомни</h1>
+  <p>Открой все зеленые клетки</p>
+    <TheBoard/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TheBoard from "@/components/Board.vue";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TheBoard,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .app {
+    margin-top: 40px;
+    text-align: center;
+    font-family: Helvetica, sans-serif;
+  }
+  .logo {
+    width: 150px;
+  }
 </style>
---
+++ src/components/Board.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="wrapper">
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "TheBoard"
+}
+</script>
+
+<style scoped>
+.wrapper {
+  width: 300px;
+  background: #eee;
+
+}
+</style>
