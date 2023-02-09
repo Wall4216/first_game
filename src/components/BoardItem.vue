eb4fa7a39f6371476f28350a82c8d26a9d97354c
Wall <ilnazamirhanov61@gmail.com>
2023-02-09 22:42:02 +0300

    add hide cells

--- src/components/BoardItem.vue
+++ src/components/BoardItem.vue
@@ -1,6 +1,6 @@
 <template>
-<span :class="'item ' + (field.value === 1 ? 'item-active' : '')">
-</span>
+  <span :class="'item ' + ((field.value === 1 && preview) ? 'item-active' : '')"></span>
+
 </template>
 
 <script>
@@ -10,8 +10,14 @@ export default {
     field: {
       type: Object,
       required: true,
+    },
+    preview: {
+      type: Boolean,
+      required: false,
+      default: false,
     }
-  }
+  },
+
 }
 </script>
 
