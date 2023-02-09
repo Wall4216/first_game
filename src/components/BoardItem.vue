40a59a7b5a61deb170121c2774e72c3641248200
Wall <ilnazamirhanov61@gmail.com>
2023-02-09 20:42:13 +0300

    add boardItem component

---
+++ src/components/BoardItem.vue
@@ -0,0 +1,22 @@
+<template>
+<span class="item">
+</span>
+</template>
+
+<script>
+export default {
+  name: "TheBoard"
+}
+</script>
+
+<style scoped>
+.item {
+position: relative;
+  width: 50px;
+  height: 50px;
+  background: #ccc;
+  display: inline-block;
+  margin: 5px;
+  cursor: pointer;
+}
+</style>
